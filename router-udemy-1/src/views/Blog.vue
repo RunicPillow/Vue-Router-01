9809ab599b154e0d057a256b2fdc7d2cb599a764
RunicPillow <86902739+RunicPillow@users.noreply.github.com>
2022-01-09 23:08:22 -0600

    Consume API

--- router-udemy-1/src/views/Blog.vue
+++ router-udemy-1/src/views/Blog.vue
@@ -1,5 +1,6 @@
 <template>
     <Titulo texto="Titulo de mi blog" />
+    <button @click="consumirApi" >Consumir API</button>
 </template>
 
 <script>
@@ -7,7 +8,25 @@ import Titulo from '../components/Titulo.vue'
 export default {
     components: {
         Titulo
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async consumirApi(){
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json() 
+                console.log(array)
+            } catch (error) {
+                console.log(error)    
+            }
+        }    
     }
+
+
 }
 </script>
 
